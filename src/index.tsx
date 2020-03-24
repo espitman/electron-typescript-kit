44f8ab3490c3593b372f9af68907458421a850b0
Saeed Heidari <saeed.heidari86@gmail.com>
2020-03-24 23:33:51 +0430

    feat: use scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
-import '@public/style.css'
+import '@public/style.scss'
 
 ReactDOM.render(
   <div className='app'>
     <h4>Welcome to React, Electron and Typescript</h4>
-    <p>Hello Saeed!</p>
+    <p>Hello Saeed!!</p>
   </div>,
   document.getElementById('app')
 )
